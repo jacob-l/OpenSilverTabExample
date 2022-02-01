f1c63c765ecad6e31bd0f0d6de857f945f878db1
Iakov Lilo <jlilo@hwdtech.com>
2022-02-01 15:59:30 +0600

    Add project files.

---
+++ OpenSilverApplication57/OpenSilverApplication57.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace OpenSilverApplication57.Simulator
+{
+    static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
